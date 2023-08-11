c018b67fdda6d3ed60da3985e032aabd5f29e7a0
Jack Maguire <49815482+BurntNail@users.noreply.github.com>
2023-08-11 16:07:43 +0100

    add more docs

--- src/array2d.rs
+++ src/array2d.rs
@@ -1,29 +1,41 @@
 use std::ops::{Index, IndexMut};
 use std::vec::IntoIter;
 
+///Structure to act as a 2D Array, generic over T.
+///
+///The array has infinite height, and we just keep adding rows as elements are added.
 #[derive(Default, Debug, Clone)]
 pub struct Array2D<T> {
     backing: Vec<T>,
     width: usize,
 }
 
+///Given a width, turn an index into an (x, y) position
 pub fn index_to_coords(width: usize, index: usize) -> (usize, usize) {
     (index / width, index % width)
 }
+///Given a width and an (x, y) position, find the index
 pub fn coords_to_index(width: usize, (x, y): (usize, usize)) -> usize {
     y * width + x
 }
 
 impl<T> Array2D<T> {
+    ///Add an element to the next available position in the grid
     pub fn add_element(&mut self, el: T) {
         self.backing.push(el);
     }
+
+    ///Remove a given index - similar to [`Vec::remove`], this will panic with an invalid index.
+    ///
+    ///NB: This also moves all elements after the chosen index left (and up) and so can change rows
     pub fn remove(&mut self, index: (usize, usize)) -> T {
         return self.backing.remove(coords_to_index(self.width, index));
     }
+    ///Get the inner vector
     pub fn to_inner(self) -> Vec<T> {
         self.backing
     }
+    ///Check whether any items fufill a condition given by a closure
     pub fn contains(&self, cnd: impl Fn(&T) -> bool) -> bool {
         self.backing.iter().any(cnd)
     }
@@ -34,6 +46,8 @@ impl<T> Array2D<T> {
     pub fn is_empty(&self) -> bool {
         self.backing.is_empty()
     }
+
+    ///Get the (columns, rows)
     pub fn size(&self) -> (usize, usize) {
         (
             self.width,
@@ -43,6 +57,7 @@ impl<T> Array2D<T> {
 }
 
 impl<T: Clone> Array2D<T> {
+    ///Create an array with given width and height filled with clones of `el`
     pub fn filled(width: usize, height: usize, el: T) -> Self {
         Self {
             backing: vec![el; width * height],
